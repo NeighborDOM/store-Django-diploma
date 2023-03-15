42810bd44a4329ecabd29a32f7d836d33591bda5
DOM <domfrost.it@gmail.com>
2023-03-15 23:47:08 +0200

    Updated ALLOWED_HOSTS

--- django_store/settings.py
+++ django_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "doms-store.onrender.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
